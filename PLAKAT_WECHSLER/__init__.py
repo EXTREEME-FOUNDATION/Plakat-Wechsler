83e83a61fe71bbbfde32146d6f1232507d62f340
Kitsune <84915532+EXTREEME-FOUNDATION@users.noreply.github.com>
2023-08-16 22:06:44 +0200

    new files

--- PLAKAT_WECHSLER/__init__.py
+++ PLAKAT_WECHSLER/__init__.py
@@ -5,10 +5,21 @@ import logging
 from logging import CRITICAL, INFO, WARNING, ERROR, DEBUG
 import json
 import configparser
+import multiprocessing
+import signal
+from types import FunctionType
+
+# IMPORT flows
+from PLAKAT_WECHSLER.flows import getstatus
+from PLAKAT_WECHSLER.flows import selftest
+from PLAKAT_WECHSLER.flows import update
+from PLAKAT_WECHSLER.flows import webserv
+
+
+from time import sleep
 
 VERSION="1.0.0"
 
-logging.basicConfig(filename="event.log",level=INFO,format="%(levelname)s - %(asctime)s:\t%(message)s")
 
 logging.info(f"Plakat-wechsler {VERSION}.\nCreated by Kelvin Maringer (https://www.mm-edv.at).")
 logging.info("Reading Config")
@@ -18,26 +29,65 @@ logging.info("Reading Config")
 with open("config/pinout.json") as s:
     global PINOUT
     PINOUT = json.loads(s.read())
+
+config = configparser.ConfigParser()
+config.read("config/config.ini")
+
+
+
+logging.info(f"Finished Reading configs:\nPinout Version::{PINOUT['VERSION']}")
+
+
+                    #2,500
+def MAXTIME(time:int) -> FunctionType|TimeoutError:
+    """Limits time a function can take"""
+    def ovrdecorator(func):
+        def hwlp():
+            raise TimeoutError
+        def wrapper(*args, **kwargs) -> any:
+            signal.signal(signal.SIGALRM,hwlp)
+            signal.alarm(time/1000)
+            try:
+                out = func(*args,**kwargs)
+                return out
+            except TimeoutError:
+                logging.warn(f"Function \"{func.__name__}\" took to long to execute. ({time/1000}s)")
+                return TimeoutError
+        return wrapper
+    return ovrdecorator
+
+
+
+
+class LightSensor:
+    def __init__(s,maxL,minL,APin) -> bool:
+        s.Value=0.0
+        s.normvals=(maxL,minL)
+        s.APin=APin
+        testok = s.update()
+        if testok in (0.0,1.0):
+            logging.error(f"Light sensor defective, value of {testok}")
+            return False
+        return True
+
+    def update(s) -> float:
+        #Code to read sensor
+        s.Value = 0.5
+        return 0.5
+    def __float__(s) -> float:
+        return s.Value
     
-logging.info(f"Finished Reading config:\nPinout Version::{PINOUT['VERSION']}")
-if VERSION != PINOUT["VERSION"]:
-    logging.warn("Pinout version does not match Wechsler version!")
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
+class Sensor:
+    def __init__(s,DPin) -> bool:
+        s.State=False
+        s.Dpin=DPin
+        testok = s.update()
+    def update(s) -> bool:
+        #Code to read sensor
+        s.State=False
+        return False
+    def __int__(s) -> bool:
+        return s.State
 
 
 
@@ -46,12 +96,42 @@ if VERSION != PINOUT["VERSION"]:
 
 
 class Motor:
-    pass
-
-
-
-
-
-class wechsler:
-    def __init__(s):
+    def __init__(s,RPin,LPin,SensU,SensD) -> False:
+        s.R=RPin
+        s.L=LPin
+        s.Direction=0# 0:off/brake | 1: clockwise | -1: counterclockwise
+        s.Poster=0 #What poster is beeing shown
         pass
+    @MAXTIME(time=2500)
+    def __ToRight(s):
+        #drive to right
+        pass
+    @MAXTIME(time=2500)
+    def __ToLeft(s):
+        #drive to left
+        pass
+    @MAXTIME(time=2500)
+    def __Stop(s):
+        #stop
+        pass
+    def ToPosterNum(s,num):
+        #drive to poster number
+        while(1):
+            if num < s.Poster:
+                s.ToLeft()
+            elif num > s.Poster:
+                s.ToRight()
+            else:
+                s.Stop()
+                break
+
+@MAXTIME(1000)
+def hh():
+    print("go")
+    sleep(10)
+    print("done")
+
+
+
+hh()
+print("done")
--- start.py
+++ start.py
@@ -1,6 +1,19 @@
+#Plakat wechsler, Created by:
+#   Kelvin Maringer (https://www.mm-edv.at)
+import logging
+logging.basicConfig(filename="event.log",level=logging.INFO,format="%(levelname)s - %(asctime)s:\t%(message)s")
+
+#imports
 import PLAKAT_WECHSLER
+import json
+VERSION="1.0.0"
+
+
 
 
 
 if __name__ == "__main__":
     pass
+    
+
+
